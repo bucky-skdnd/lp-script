54e53c433723accce1c2b26fa757e55495a9d384
bucky-skdnd <144748684+bucky-skdnd@users.noreply.github.com>
2025-11-12 15:06:51 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const channel = new BroadcastChannel("betterliving");
+const { port1, port2 } = new MessageChannel();
 
-channel.onmessage = onmessage;
+ window.addEventListener('message', onMessage);
 
 document.body.addEventListener('click', () => {
    window.parent.postMessage({ message: 'Hello whos there' }, '*');
@@ -10,10 +10,11 @@ document.body.addEventListener('click', () => {
 // Handle messages received on port2
 function onMessage(e) {
   alert(JSON.stringify(e.data));
-  channel.postMessage(`Message received by IFrame: "${e.data}"`);
+  // channel.postMessage(`Message received by IFrame: "${e.data}"`, '*');
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
- channel.postMessage("DOM fully loaded and parsed");
+ // channel.postMessage("DOM fully loaded and parsed", '*');
+ window.parent.postMessage({ message: 'DOMContentLoaded' }, '*');
 });
 
