a71d8572fc2d641215d84ad44ca099fcb48173a5
bucky-skdnd <144748684+bucky-skdnd@users.noreply.github.com>
2025-11-12 13:23:35 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
-document.body.addEventListener('click', () => {
-  window.alert('test')})
+// document.body.addEventListener('click', () => {
+//   window.alert('test')})
+let parentFramePort;
+
+// Setup the transferred port
+function initPort(e) {
+  if (e.origin !== "localhost:3000") {
+    return;
+  }
+
+  [parentFramePort] = e.ports || [];
+
+  if (!parentFramePort) {
+    return;
+  }
+
+  // parentFramePort = e.ports[0];
+  parentFramePort.onmessage = onMessage;
+}
+
+// Handle messages received on port2
+function onMessage(e) {
+  alert(JSON.stringify(e.data));
+  parentFramePort.postMessage(`Message received by IFrame: "${e.data}"`);
+}
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
+  alert("DOM fully loaded and parsed");
+  
+  // Listen for the initial port transfer message
+  window.addEventListener("message", initPort);
 });
