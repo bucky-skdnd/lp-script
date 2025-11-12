f106ba9a40ee2b3e0c4cfb89e62ea4c95fe09c02
bucky-skdnd <144748684+bucky-skdnd@users.noreply.github.com>
2025-11-12 15:50:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -14,9 +14,18 @@ window.addEventListener('message', (event) => {
 
 // Handle messages received on port2
 function onMessage(e) {
-  // console.log('Message from parent:', e.data);
+  console.log('Message from parent:', e.data);
   // alert(JSON.stringify(e.data));
   // channel.postMessage(`Message received by IFrame: "${e.data}"`, '*');
+switch (event.data.type) {
+  case 'getIframeHeight':
+    const container = document.querySelector('[data-height]');
+    iframePort.postMessage({type: 'setIframeHeight', data: { height: container.dataset.height }});
+    break;
+  default:
+    console.log('default');
+    break;
+}
 }
 
 document.addEventListener("load", () => {
