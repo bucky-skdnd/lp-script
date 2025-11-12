ba17b55adcb22f3d3060aa1e915ee21fcb7a703e
bucky-skdnd <144748684+bucky-skdnd@users.noreply.github.com>
2025-11-12 13:10:18 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
 document.body.addEventListener('click', () => {
   window.alert('test')})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+});
