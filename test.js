77189050d9fcfdb556e5ec84166120089e034096
bucky-skdnd <144748684+bucky-skdnd@users.noreply.github.com>
2025-11-12 15:16:23 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,20 @@
-
+window.iframePort = null;
 
 window.addEventListener('message', (event) => {
     if (event.data === 'init' && event.ports.length > 0) {
-       const iframePort = event.ports[0];
+       iframePort = event.ports[0];
        // Now the iframe can use iframePort to send and receive messages
-       iframePort.onmessage = (messageEvent) => {
-           console.log('Message from parent:', messageEvent.data);
-       };
-       iframePort.postMessage('Hello from iframe!');
+       iframePort.onmessage = onMessage;
    }
 });
 
 document.body.addEventListener('click', () => {
-   window.parent.postMessage({ date: new Date() }, '*');
+   iframePort.postMessage({ date: new Date() }, '*');
 })
-
 
 // Handle messages received on port2
 function onMessage(e) {
+    console.log('Message from parent:', messageEvent.data);
   alert(JSON.stringify(e.data));
   // channel.postMessage(`Message received by IFrame: "${e.data}"`, '*');
 }
